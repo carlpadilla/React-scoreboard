ebb55a8f76ef163b817c28c752c97f75b12d19f3
carlpadilla <carlpadilla@protonmail.com>
2019-07-29 19:01:06 -0400

    iterating over object to render players to dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
 import Players from './components/Players';
-
 import './App.css';
 
-function App() {
+const players = [
+  { name: 'Carl Padilla', score: 14 },
+  {
+    name: 'Jenny Moua',
+    score: 69
+  },
+  {
+    name: 'Nina Moua',
+    score: 10
+  }
+];
+
+function App(props) {
   return (
     <div className='App'>
-      <Header title='Scoreboard' totalPlayers={1} />
-      <Players playerName='Carl Padilla' score={0} />
-      <Players playerName='Jenny Moua' score={10} />
+      <Header title='Scoreboard' totalPlayers={players.length} />
+      {players.map(plyr => {
+        return <Players playerName={plyr.name} score={plyr.score} />;
+      })}
     </div>
   );
 }
