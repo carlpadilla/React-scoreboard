ca6d1f445bb4d07b2509061403eba880dcc46d7b
carlpadilla <carlpadilla@protonmail.com>
2019-07-29 18:05:25 -0400

    props have been iplemented into the header comoponent

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <Header />
+      <Header title='Scoreboard' totalPlayers={1} />
       <Players />
     </div>
   );
