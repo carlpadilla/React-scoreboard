1fd3a3189af1016ac276ce26a192e8ce412b334b
carlpadilla <carlpadilla@protonmail.com>
2019-08-08 01:35:39 -0400

    timer function is now counting up in seconds, start and reset button are functional

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Stopwatch extends React.Component {
+class Stopwatch extends Component {
+  state = {
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
+  };
+
+  componentDidMount() {
+    this.setIntervalID = setInterval(() => this.tick(), 100);
+  }
+
+  tick = () => {
+    // console.log('ticking');
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState(prevState => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+      }));
+    }
+  };
+
+  handleStopwatch = () => {
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+    if (!this.state.isRunning) {
+      //   console.log('starting');
+      this.setState({ previousTime: Date.now() });
+    }
+  };
+
+  handleReset = () => {
+    this.setState({ elapsedTime: 0 });
+  };
+
   render() {
+    const time = Math.floor(this.state.elapsedTime / 1000);
     return (
       <div className='stopwatch'>
         <h2>Stopwatch</h2>
-        <span className='stopwatch-time'>0</span>
-        <button>Start</button>
-        <button>Stop</button>
+        <span className='stopwatch-time'>{time}</span>
+
+        <button onClick={this.handleStopwatch}>
+          {this.state.isRunning ? 'Stop' : 'Start'}
+        </button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
